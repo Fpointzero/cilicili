1d0406b99f7f0cd9a75d882c233ee4f786928548
carefree <781381449@qq.com>
2024-01-04 16:40:12 +0800

    videomapper SearchServlet

--- src/main/java/xyz/fpointzero/mapper/VideoMapper.java
+++ src/main/java/xyz/fpointzero/mapper/VideoMapper.java
@@ -3,6 +3,8 @@ package xyz.fpointzero.mapper;
 import org.apache.ibatis.annotations.Param;
 import xyz.fpointzero.model.Video;
 
+import java.util.List;
+
 public interface VideoMapper {
-    Video getByTitle(@Param("keyword")String keyword);
+    List<Video> getByTitle(@Param("keyword")String keyword);
 }
