ecaec911efcdbac499a2b685f756f1d69ab17db2
carefree <781381449@qq.com>
2024-01-12 14:06:47 +0800

    updateHistory

--- src/main/java/xyz/fpointzero/mapper/HistoryMapper.java
+++ src/main/java/xyz/fpointzero/mapper/HistoryMapper.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface HistoryMapper {
     List<History> getHistoryList(Integer uid);
     void setHistory(@Param("uid")Integer uid, @Param("vid")Integer vid);
-    void updateHistory(@Param("uid")Integer uid, @Param("vid")Integer vid);
+    void updateHistory(@Param("uid") Integer uid, @Param("vid") Integer vid);
     void delHistory(@Param("uid")Integer uid, @Param("vid")Integer vid);
     void delAll(@Param("uid")Integer uid);
 }
